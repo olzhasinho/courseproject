79b52e666e41e42ad9b38a0f53900d8945a6f781
olzhasinho <zhas1ko@bk.ru>
2020-05-01 03:01:35 +0600

    first commit

---
+++ Models/ArtRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtShop3.Models
+{
+    public class ArtRepository : IArtRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ArtRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Art> AllArts
+        {
+            get
+            {
+                return _appDbContext.Arts.Include(a => a.Category);
+            }
+        }
+
+        public IEnumerable<Art> ArtsOfTheWeek
+        {
+            get
+            {
+                return _appDbContext.Arts.Include(a => a.Category).Where(a => a.IsArtOfTheWeek);
+            }
+        }
+
+        public Art GetArtById(int artId)
+        {
+            return _appDbContext.Arts.FirstOrDefault(a => a.ArtId == artId);
+        }
+    }
+}
---
+++ Models/IArtRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtShop3.Models
+{
+    public interface IArtRepository
+    {
+        IEnumerable<Art> AllArts { get; }
+        IEnumerable<Art> ArtsOfTheWeek { get; }
+
+        Art GetArtById(int artId);
+
+    }
+}
